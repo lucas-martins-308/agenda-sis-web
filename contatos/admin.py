12d2cc01e6f41d3ddbc3824e7aeeec54f39a7580
LUCAS DE ANDRADE MARTINS <lucas_de-martins@estudante.sesisenai.org.br>
2025-03-17 21:23:48 -0300

    Add gitignore e requirements

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Contato
+from .models import Contato, Categoria
+
+admin.site.register(Categoria)
 
 @admin.register(Contato)
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'email', 'endereco')
     search_fields = ('nome', 'email', 'telefone')
     list_filter = ('nome',)
+    
--- contatos/models.py
+++ contatos/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     telefone = models.CharField(max_length=20)
     email = models.EmailField()
     endereco = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
