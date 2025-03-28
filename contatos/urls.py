6786500139c19c0322498e797b6bba2f462db8df
LUCAS DE ANDRADE MARTINS <lucasmartins308@gmail.com>
2025-03-28 20:34:15 -0300

    corrigindo gitignore e adicionando form de contatos no front

--- contatos/urls.py
+++ contatos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, lista_contatos, detalhe_contato
+from .views import home, lista_contatos, detalhe_contato, add_contato
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
     path('contatos/', lista_contatos, name='lista_contatos'),
     path('contatos/<int:id>/', detalhe_contato, name='detalhe_contato'),
+    path('add_contato/', add_contato, name='add_contato')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Contato
+from .models import Contato, Categoria
 from django.contrib.auth.decorators import login_required
 
 def home(request):
@@ -15,3 +15,23 @@ def lista_contatos(request):
 def detalhe_contato(request, id):
     contato = Contato.objects.filter(id=id).first() 
     return render(request, 'contatos/detalhe_contato.html', {'contato': contato})
+
+@login_required(login_url='login')
+def add_contato(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+        endereco = request.POST.get('endereco')
+        categoria = request.POST.get('categoria')
+
+        categoria_id = Categoria.objects.get(id=categoria)
+        Contato.objects.create(
+            nome=nome,
+            telefone=telefone,
+            email=email,
+            endereco=endereco,
+            categoria_id=categoria
+        )
+    categorias = Categoria.objects.all() 
+    return render(request, 'contatos/add_contato.html', {'categorias': categorias})
